18965766e4ad6b9e4881ee2ef0b21e443084bbb2
chiilog <purgexpurge@gmail.com>
2025-02-07 13:55:53 +0900

    BrowserRouter -> HastRouterに置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -22,12 +22,12 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </QueryClientProvider>
 );
