e0b348d94de1d7e54777c5d3d766079234c0c699
chiilog <purgexpurge@gmail.com>
2025-04-10 14:43:39 +0900

    shadcn追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
+    exclude: ['src/lib/**/*', 'src/components/ui/**/*'],
+    passWithNoTests: true,
   },
 });
